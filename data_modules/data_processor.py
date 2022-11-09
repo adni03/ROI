71e4f542bd74bfc0bcfa39b93e7334cee1ca0611
Devin Ulam <68881829+golf-n-code@users.noreply.github.com>
2022-11-09 08:39:47 -0500

    perf tests

--- data_modules/data_processor.py
+++ data_modules/data_processor.py
@@ -1,7 +1,11 @@
 import pandas as pd
 
 def loadCSV(year):
+    # all columns (raw)
     df = pd.read_csv("https://www.dropbox.com/s/jzra8h8ahesqcys/MERGED" + str(year) + "_01_PP.csv?raw=1")
     return df
 
-df = loadCSV(2000)
+def loadSkinnyCSV():
+    # only 25 columns
+    df = pd.read_csv("https://www.dropbox.com/s/jzra8h8ahesqcys/test.csv?raw=1")
+    return df
--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import data_modules.data_processor as data_processor
 
-df2000 = data_processor.loadCSV(2000)
+df = data_processor.loadSkinnyCSV() #17s
+df = data_processor.loadCSV(2000) #17.2s
+
+dfList = []
+for year in range(1996,2020):
+    try:
+        df = data_processor.loadCSV(year)
+        dfList.append(df)
+    except:
+        print("error processing data for year " + str(year))
