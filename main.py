436eab962a467722455b250568f33f843309f9f1
adni03 <srisai@student.nitw.ac.in>
2022-11-19 01:08:54 -0500

    bit of code to test preprocessor script

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-import data_modules.data_processor as data_processor
+# import data_modules.data_processor as data_processor
+#
+# df = data_processor.loadCSV(2000) #17.2s
+# df = data_processor.loadSkinnyCSV() #0.5s
+#
+# dfList = []
+# for year in range(1996,2020):
+#     try:
+#         df = data_processor.loadCSV(year)
+#         dfList.append(df)
+#     except:
+#         print("error processing data for year " + str(year))
 
-df = data_processor.loadCSV(2000) #17.2s
-df = data_processor.loadSkinnyCSV() #0.5s
+import data_modules.data_preprocessor as DP
 
-dfList = []
-for year in range(1996,2020):
-    try:
-        df = data_processor.loadCSV(year)
-        dfList.append(df)
-    except:
-        print("error processing data for year " + str(year))
+dp = DP.DataPreprocessor(0.2)
+df = dp.preprocess_data()
