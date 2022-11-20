20750f2bfa95d81def821bced51b452fe7b8cd16
Devin Ulam <dulam@energex-tech.com>
2022-11-19 19:09:49 -0500

    Starting point - need to remove insignificance

--- models/roi_predictor.py
+++ models/roi_predictor.py
@@ -2,9 +2,10 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+import statsmodels.api as sm
 
 # def __init__(self, region, sat_score,
 #                  act_score, funding_type,
@@ -29,32 +30,20 @@ scorecard_working.drop(["Geography"],axis=1,inplace=True)
 # Split train and test
 np.random.seed(0)
 df_train, df_test = train_test_split(scorecard_working,train_size=0.7, random_state=100)
-x_train = df_train.loc[:,df_train.columns != "ROI"]
-y_train = df_train["ROI"]
-x_test = df_test.loc[:,df_test.columns != "ROI"]
-y_test = df_test["ROI"]
+y_train = df_train.pop("ROI")
+x_train = df_train
+y_test = df_test.pop("ROI")
+x_test = df_test
 
-# Pipeline for feature scaling (standardization) and fit
-pipeline = Pipeline([
-    ("StdScaling", StandardScaler()),
-    ("LinReg", LinearRegression())
-])
+# Feature Scaling
+train_cols = x_train.columns
+scaler = StandardScaler()
+x_train = pd.DataFrame(scaler.fit_transform(x_train), columns=train_cols)
+test_cols = x_test.columns
+x_test = pd.DataFrame(scaler.fit_transform(x_test), columns=test_cols)
 
-pipeline.fit(x_train,y_train)
-
-y_pred = pipeline.predict(x_test)
-
-plt.figure(figsize=(10,10))
-plt.scatter(y_test, y_pred, c='crimson')
-
-p1 = max(max(y_pred), max(y_test))
-p2 = min(min(y_pred), min(y_test))
-plt.plot([p1, p2], [p1, p2], 'b-')
-plt.xlabel('True Values', fontsize=15)
-plt.ylabel('Predictions', fontsize=15)
-plt.axis('equal')
-plt.show()
-
-regression_results = pipeline.named_steps['LinReg']
-
-regression_results.
+# Fit
+x_train_lm = sm.add_constant(x_train)
+y_train_lm = y_train.values.reshape(-1,1)
+lm = sm.OLS(y_train_lm, x_train_lm).fit()
+lm.summary()
