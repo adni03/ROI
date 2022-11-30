916bc34d28bb2bcbb9360d8c261f82fa0e776a4c
adni03 <srisai@student.nitw.ac.in>
2022-11-30 16:23:12 -0500

    added code for more PCIPs

--- data_modules/data_preprocessor.py
+++ data_modules/data_preprocessor.py
@@ -12,6 +12,10 @@ class DataPreprocessor:
         with open('../data/columns.txt', 'r') as r:
             for column in r:
                 self.interesting_columns.append(column.strip())
+        for i in range(10, 55):
+            if i not in [17, 18, 20, 21, 28, 32, 33, 34, 35, 36, 37, 53]:
+                self.interesting_columns.append(f'PCIP{str(i)}')
+
 
         self.OGFILE = '../data/MERGED2019.csv'
         self.CLEANFILE = '../data/downloaded_clean_2019.csv'
