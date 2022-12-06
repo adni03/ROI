1e6405b490977188f5c75364931461f197426270
adni03 <srisai@student.nitw.ac.in>
2022-12-06 16:10:18 -0500

    optimized imports

--- models/roi_predictor_v2.py
+++ models/roi_predictor_v2.py
@@ -1,11 +1,10 @@
 # -----------------------------
-import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+import pandas as pd
+import shap
 from sklearn.compose import ColumnTransformer
 from sklearn.ensemble import RandomForestRegressor
-import shap
+from sklearn.preprocessing import StandardScaler
 
 
 # -----------------------------
